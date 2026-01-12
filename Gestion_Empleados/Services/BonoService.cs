73abbb0ae200a511b4a04edb92092a1436802498
Abel Poveda <abelpoveda@gmail.com>
2026-01-12 12:10:41 +0100

    Agregado proyecto xUnit

---
+++ Gestion_Empleados/Services/BonoService.cs
@@ -0,0 +1,14 @@
+﻿namespace Gestion_Empleados.Services
+{
+    public class BonoService
+    {
+        public decimal CalcularBono(decimal salario, string puesto)
+        {
+            if (puesto == "Jefe")
+            {
+                return salario * 0.10m;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Gestión_Empleados.Tests/BonoServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Gestion_Empleados.Services;
+
+namespace Gestión_Empleados.Tests
+{
+    public class BonoServiceTests
+    {
+        [Fact]
+        public void CalcularBono_CuandoEsJefe_DiezPorCiento()
+        {
+            var service = new BonoService();
+            var salario = 1000m;
+            var puesto = "Jefe";
+
+            var resultado = service.CalcularBono(salario, puesto);
+
+            Assert.Equal(100, resultado);
+        }
+
+        [Fact]
+        public void CalcularBono_CuandoNoEsJefe_Cero()
+        {
+            var service = new BonoService();
+            var salario = 1000m;
+            var puesto = "Programador";
+
+            var resultado = service.CalcularBono(salario, puesto);
+
+            Assert.Equal(0, resultado);
+        }
+    }
+}
